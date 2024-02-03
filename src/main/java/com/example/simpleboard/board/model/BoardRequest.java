2c3c3c6b045fc75bb831e43e006895e949a40316
seeeop2 <seeeop2@gmail.com>
2024-02-03 19:10:45 +0900

    게시판 서비스 및 dto 추가
    게시판 생성 메소드를 작성함.

---
+++ src/main/java/com/example/simpleboard/board/model/BoardRequest.java
@@ -0,0 +1,21 @@
+package com.example.simpleboard.board.model;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.*;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class  BoardRequest {
+
+    @NotBlank
+    private String boardName;
+
+}
