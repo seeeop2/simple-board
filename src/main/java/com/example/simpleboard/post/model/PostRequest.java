672ffd208e5b0bfa01c02db364a85038e0d9a38c
seeeop2 <seeeop2@gmail.com>
2024-02-03 19:13:10 +0900

    게시글 서비스 및 dto 추가
    게시글 생성 메소드를 작성함.
    * create 메소드 내 boardId의 경우, API 테스트를 위해 1L로 임시 고정 시킴.

---
+++ src/main/java/com/example/simpleboard/post/model/PostRequest.java
@@ -0,0 +1,38 @@
+package com.example.simpleboard.post.model;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.*;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class PostRequest {
+
+    @NotBlank
+    private String userName;
+
+    @Size(min = 4 , max = 4)
+    @NotBlank
+    private String password;
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String title;
+
+    @NotBlank
+    private String content;
+
+}
