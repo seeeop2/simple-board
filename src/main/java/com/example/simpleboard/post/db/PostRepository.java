d9bad211fc1a4231243b9cc518af4abee26d2173
seeeop2 <seeeop2@gmail.com>
2024-02-06 14:53:34 +0900

    게시글 Repository, Service 수정
    게시글 열람, 전체 게시글 열람, 게시글 삭제 메소드 추가
    * 게시글을 삭제하면, DB에서 삭제되는 것이 아니고 Status를 변경함.
    * 삭제된 게시글을 열람하면, 보이지 않게 하기 위해서 repository에 메소드를 추가함.

--- src/main/java/com/example/simpleboard/post/db/PostRepository.java
+++ src/main/java/com/example/simpleboard/post/db/PostRepository.java
@@ -3,6 +3,11 @@ package com.example.simpleboard.post.db;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PostRepository extends JpaRepository<PostEntity,Long> {
+
+    // select * from post where id =? and status = ? order by id desc limit 1
+    Optional<PostEntity> findFirstByIdAndStatusOrderByIdDesc(Long id, String status);
 }
--- src/main/java/com/example/simpleboard/post/service/PostService.java
+++ src/main/java/com/example/simpleboard/post/service/PostService.java
@@ -3,10 +3,12 @@ package com.example.simpleboard.post.service;
 import com.example.simpleboard.post.db.PostEntity;
 import com.example.simpleboard.post.db.PostRepository;
 import com.example.simpleboard.post.model.PostRequest;
+import com.example.simpleboard.post.model.PostViewRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -30,4 +32,43 @@ public class PostService {
     }
 
 
+    public PostEntity view(PostViewRequest postViewRequest) {
+        return postRepository.findFirstByIdAndStatusOrderByIdDesc(postViewRequest.getPostId(),"REGISTERED")
+                .map( it -> {
+                    //entity존재
+                    if (!it.getPassword().equals(postViewRequest.getPassword())){
+
+                        String format = "패스워드가 맞지 않습니다 %s vs %s";
+
+                        throw new RuntimeException(String.format(format, it.getPassword() , postViewRequest.getPassword()));
+                    }
+                    return it;
+                }).orElseThrow(() -> {
+                    return new RuntimeException("해당 게시글이 존재 하지 않습니다 : " + postViewRequest.getPostId());
+                });
+    }
+
+    public List<PostEntity> all() {
+        return postRepository.findAll();
+    }
+
+    public void delete(PostViewRequest postViewRequest) {
+        postRepository.findById(postViewRequest.getPostId())
+                .map( it -> {
+                    //entity존재
+                    if (!it.getPassword().equals(postViewRequest.getPassword())){
+
+                        String format = "패스워드가 맞지 않습니다 %s vs %s";
+
+                        throw new RuntimeException(String.format(format, it.getPassword() , postViewRequest.getPassword()));
+                    }
+                    it.setStatus("UNREGISTERED");
+                    postRepository.save(it);
+                    return it;
+
+                }).orElseThrow(() -> {
+                    return new RuntimeException("해당 게시글이 존재 하지 않습니다 : " + postViewRequest.getPostId());
+                });
+
+    }
 }
