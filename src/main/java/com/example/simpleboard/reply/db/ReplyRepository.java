664d7e4c02de3ae253eb3f72c568e3074b39f447
seeeop2 <seeeop2@gmail.com>
2024-02-09 20:16:55 +0900

    댓글 repository 메소드 추가 및 service 추가
    댓글 서비스를 추가하고, 특정 게시글에 상태가 'REGISTERED'인 댓글들을 조회하는 메소드를 만듦.

--- src/main/java/com/example/simpleboard/reply/db/ReplyRepository.java
+++ src/main/java/com/example/simpleboard/reply/db/ReplyRepository.java
@@ -3,6 +3,12 @@ package com.example.simpleboard.reply.db;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ReplyRepository extends JpaRepository<ReplyEntity,Long> {
+
+    //select * from reply where post_id=? and status= ? order by id desc
+    List<ReplyEntity> findAllByPostIdAndStatusOrderByIdDesc(Long postId, String status);
+
 }
