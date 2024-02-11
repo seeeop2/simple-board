7351475be54c0c1517dde575807deeefdb68062e
seeeop2 <seeeop2@gmail.com>
2024-02-12 02:59:49 +0900

    게시판, 게시글 converter 작성
    Entity를 매개변수로 받아 Dto로 반환해주는 Converter를 작성함.

---
+++ src/main/java/com/example/simpleboard/board/service/BoardConverter.java
@@ -0,0 +1,31 @@
+package com.example.simpleboard.board.service;
+
+import com.example.simpleboard.board.db.BoardEntity;
+import com.example.simpleboard.board.model.BoardDto;
+import com.example.simpleboard.post.model.PostDto;
+import com.example.simpleboard.post.service.PostConverter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class BoardConverter {
+
+    private final PostConverter postConverter;
+
+    public BoardDto toDto(BoardEntity boardEntity){
+        List<PostDto> postList = boardEntity.getPostList().stream().map(postEntity -> {
+            return postConverter.toDto(postEntity);
+        }).collect(Collectors.toList());
+
+        BoardDto.builder()
+                .id(boardEntity.getId())
+                .boardName(boardEntity.getBoardName())
+                .status(boardEntity.getStatus())
+                .postList(postList)
+                .build();
+    }
+}
