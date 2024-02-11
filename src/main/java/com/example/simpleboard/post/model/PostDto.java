17ee6d2ba1d942d0a34eb0447e433d4b91645632
seeeop2 <seeeop2@gmail.com>
2024-02-12 02:57:29 +0900

    게시판, 게시글 Dto(response) 추가
    엔티티를 반환하면 나중에 문제가 될 수 있으니,
    dto로 반환하기 위해 추가함.

---
+++ src/main/java/com/example/simpleboard/post/model/PostDto.java
@@ -0,0 +1,38 @@
+package com.example.simpleboard.post.model;
+
+import com.example.simpleboard.reply.db.ReplyEntity;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class PostDto {
+
+    private Long id;
+
+    private Long boardId;
+
+    private String userName;
+
+    private String password;
+
+    private String email;
+
+    private String status;
+
+    private String title;
+
+    private String content;
+
+    private LocalDateTime postedAt;
+
+}
