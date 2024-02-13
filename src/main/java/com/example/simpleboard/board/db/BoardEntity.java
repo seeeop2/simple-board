c54ad0f2f9a06881e6813e4210c9048b03020da4
seeeop2 <seeeop2@gmail.com>
2024-02-13 17:24:46 +0900

    게시판 엔티티의 게시글 리스트 옵션 변경
    Status가 'REGISTERED'인 게시글만 불러올 것이며,
    게시글 id를 기준으로 내림차순 정렬.

--- src/main/java/com/example/simpleboard/board/db/BoardEntity.java
+++ src/main/java/com/example/simpleboard/board/db/BoardEntity.java
@@ -2,6 +2,7 @@ package com.example.simpleboard.board.db;
 
 import com.example.simpleboard.post.db.PostEntity;
 import lombok.*;
+import org.hibernate.annotations.Where;
 
 import javax.persistence.*;
 import java.util.List;
@@ -23,6 +24,8 @@ public class BoardEntity {
 
     private String status;
 
+    @OrderBy("id desc")
+    @Where(clause = "status = 'REGISTERED'")
     @OneToMany(mappedBy ="board")
     private List<PostEntity> postList = List.of();
 
