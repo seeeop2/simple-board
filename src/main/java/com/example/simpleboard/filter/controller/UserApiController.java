2f928e90ea256740809274ea9b9ef6fd5aaf73a5
seeeop2 <seeeop2@gmail.com>
2024-02-15 17:41:05 +0900

    사용자 컨트롤러 작성
    filter 예제를 위하여 임시로 등록 API 작성함.
    DB에 탑재 되지는 않음.

---
+++ src/main/java/com/example/simpleboard/filter/controller/UserApiController.java
@@ -0,0 +1,21 @@
+package com.example.simpleboard.filter.controller;
+
+import com.example.simpleboard.filter.model.UserRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequestMapping("/api/user")
+@RestController
+public class UserApiController {
+
+    @PostMapping("")
+    public UserRequest register(@RequestBody UserRequest userRequest){
+        log.info("{}",userRequest);
+        return userRequest;
+    }
+
+}
