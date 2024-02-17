1d4133c14ca54242e01726adde64c9bc7301dc0f
seeeop2 <seeeop2@gmail.com>
2024-02-17 16:25:36 +0900

    사용자 컨트롤러 인터셉터 적용
    이전 커밋에서 작성한 @OpenApi를 이용하여 인터셉터 적용되는지 확인함.
    * Method 위에 어노테이션을 이용하면 해당 메소드만 인터셉터 적용된다.
    * Class 위에 어노테이션을 이용하면 해당 클래스가 인터셉터 적용된다.

--- src/main/java/com/example/simpleboard/filter/controller/UserApiController.java
+++ src/main/java/com/example/simpleboard/filter/controller/UserApiController.java
@@ -1,21 +1,24 @@
 package com.example.simpleboard.filter.controller;
 
+import com.example.simpleboard.filter.interceptor.OpenApi;
 import com.example.simpleboard.filter.model.UserRequest;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RequestMapping("/api/user")
 @RestController
 public class UserApiController {
 
+    @OpenApi
     @PostMapping("")
     public UserRequest register(@RequestBody UserRequest userRequest){
         log.info("{}",userRequest);
         return userRequest;
+    }
+    @GetMapping("/hello")
+    public void hello(){
+        log.info("hello");
     }
 
 }
