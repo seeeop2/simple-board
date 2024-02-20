cccbbbe01147227ce1e1646afa4ac730d9f24652
seeeop2 <seeeop2@gmail.com>
2024-02-21 00:02:43 +0900

    Filter, Interceptor 적용 해제
    Spring AOP 예제를 위하여,
    이전에 적용되었던 Filter 와 Interceptor가 실행되지 않도록 주석 처리함.

--- src/main/java/com/example/simpleboard/filter/filter/LoggerFilter.java
+++ src/main/java/com/example/simpleboard/filter/filter/LoggerFilter.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletResponseWrapper;
 import java.io.IOException;
 
 @Slf4j
-@Component
+//@Component
 public class LoggerFilter implements Filter {
 
     @Override
