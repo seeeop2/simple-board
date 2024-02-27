28a8a028ad06cd9246c3d84557e3e81be3bd6951
seeeop2 <seeeop2@gmail.com>
2024-02-28 00:23:21 +0900

    댓글 DTO 생성
    추상화한 댓글 기능을 사용해보기 위해 ReplyDto를 생성함.

---
+++ src/main/java/com/example/simpleboard/reply/model/ReplyDto.java
@@ -0,0 +1,45 @@
+package com.example.simpleboard.reply.model;
+
+import com.example.simpleboard.post.db.PostEntity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+
+public class ReplyDto {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private Long postId;
+
+    private String userName;
+
+    private String password;
+
+    private String status;
+
+    private String title;
+
+    private String content;
+
+    private LocalDateTime repliedAt;
+
+
+
+}
