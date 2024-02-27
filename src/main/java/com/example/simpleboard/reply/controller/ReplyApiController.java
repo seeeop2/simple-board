fb012116af674f56910971a73a42a39839b69aaa
seeeop2 <seeeop2@gmail.com>
2024-02-28 00:26:57 +0900

    댓글 Controller 추상화
    CRUD 기능을 탑재한 추상화된 Controller로 변경함.

--- src/main/java/com/example/simpleboard/reply/controller/ReplyApiController.java
+++ src/main/java/com/example/simpleboard/reply/controller/ReplyApiController.java
@@ -1,6 +1,8 @@
 package com.example.simpleboard.reply.controller;
 
+import com.example.simpleboard.crud.CRUDAbstractApiController;
 import com.example.simpleboard.reply.db.ReplyEntity;
+import com.example.simpleboard.reply.model.ReplyDto;
 import com.example.simpleboard.reply.model.ReplyRequest;
 import com.example.simpleboard.reply.service.ReplyService;
 import lombok.RequiredArgsConstructor;
@@ -14,12 +16,14 @@ import javax.validation.Valid;
 @RequiredArgsConstructor
 @RequestMapping("/api/reply")
 @RestController
-public class ReplyApiController {
+public class ReplyApiController extends CRUDAbstractApiController<ReplyDto,ReplyEntity> {
 
+/* 추상화를 위해 주석 처리
     private final ReplyService replyService;
 
     @PostMapping("")
     public ReplyEntity create(@Valid @RequestBody ReplyRequest replyRequest){
         return replyService.create(replyRequest);
     }
+*/
 }
